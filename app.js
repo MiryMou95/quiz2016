b707660e371b3cac1d26fa0a82b10f061995e912
MiryMou95 <m.mourino@alumnos.upm.es>
2016-05-02 20:34:12 +0200

    Tema 13: Mensajes de Flash

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var session = require('express-session');
 var partials = require('express-partials');
+var flash = require('express-flash');
 var routes = require('./routes/index');
 
 var app = express();
@@ -21,6 +22,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(session({ secret: "Quiz 2016", resave: false, saveUninitialized: true }));
+app.use(flash());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
