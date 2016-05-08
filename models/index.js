e06c6b535a9d35f040f725f377a139e32463602c
MiryMou95 <m.mourino@alumnos.upm.es>
2016-05-08 18:58:36 +0200

    Tema 18: Crear comentarios

--- models/index.js
+++ models/index.js
@@ -23,5 +23,13 @@ var sequelize = new Sequelize(url, { storage: storage, omitNull: true });
 // Importar la definición de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 
+// Importar la definición de la tabla Comments de comment.js
+var Comment = sequelize.import(path.join(__dirname,'comment'));
+
+// Relaciones entre modelos
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz; // Exportar definición de la tabla Quiz
+exports.Comment = Comment; // Exportar definición de la tabla Comments
 
