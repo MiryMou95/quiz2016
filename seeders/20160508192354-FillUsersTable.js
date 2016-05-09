08f311ba5a7138ffee01b238de7a61353a571448
MiryMou95 <m.mourino@alumnos.upm.es>
2016-05-09 20:40:51 +0200

    Tema 20: Gestión de usuarios

--- seeders/20160508192354-FillUsersTable.js
+++ seeders/20160508192354-FillUsersTable.js
@@ -7,7 +7,7 @@ function encryptPassword(password,salt){
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Users', [ { username: 'admin', password: encryptPassword('1234', 'aaaa'), salt: 'aaaa', isAdmin: true, createdAt: new Date(), updatedAt: new Date() }, { username: 'pepe', password: encryptPassword('5678', 'bbbb'), salt: 'bbbb', isAdmin: true, createdAt: new Date(), updatedAt: new Date() } ] );
+    return queryInterface.bulkInsert('Users', [ { username: 'admin', password: encryptPassword('1234', 'aaaa'), salt: 'aaaa', isAdmin: true, createdAt: new Date(), updatedAt: new Date() }, { username: 'pepe', password: encryptPassword('5678', 'bbbb'), salt: 'bbbb', createdAt: new Date(), updatedAt: new Date() } ] );
   },
 
   down: function (queryInterface, Sequelize) {
